2f756f33593cf15512c8abe803b837446c29f3d6
Jason <182464121+latoiste@users.noreply.github.com>
2026-02-12 20:13:11 +0700

    teehee

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using WindowsMediaController;
+namespace OneSound;
+
+public class Program
+{
+    public static void Main()
+    {
+        var manager = new MediaManager();
+        manager.Start();
+        
+        var mediaSesions = manager.CurrentMediaSessions;
+        foreach (var session in mediaSesions.Keys)
+        {
+            Console.WriteLine(session);
+        }
+        //GOOD ENOGUH
+        Console.ReadLine();
+
+        manager.Dispose();
+    }
+}
